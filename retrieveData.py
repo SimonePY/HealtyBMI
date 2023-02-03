32a067d20be1f7c88cef33770556531c7f08574e
SimonePY <92288879+SimonePY@users.noreply.github.com>
2023-02-03 03:05:20 +0100

    More updates
    - Code optimized.
    
    - Created method for generate the db files along with the table and columns.
    
    - Added name input

--- retrieveData.py
+++ retrieveData.py
@@ -1,23 +1,22 @@
 import sqlite3
+from typing import Union, List, Dict, Any
 
 
-def retrieve() -> str:
+def retrieve() -> Union[str, List[Dict[str, Any]]]:
     try:
         with sqlite3.connect("result.db") as database:
             cursor = database.cursor()
             cursor.execute("SELECT * FROM bmiResult")
             columns = [column[0] for column in cursor.description]
             results = [dict(zip(columns, row)) for row in cursor.fetchall()]
-            if not results:
-                return "No Data."
-            else:
-                return results
+            return "No Data." if not results else results
     except sqlite3.OperationalError:
         return "Error: database file not found."
 
 
-def print_results(results) -> str:
+def print_results(results: List[Dict[str, Any]]) -> None:
     for result in results:
+        print(f"Name : {result['name']}")
         print(f"Date: {result['datetime']}")
         print(f"Weight: {result['weight']}")
         print(f"Height: {result['height']}")
@@ -26,7 +25,7 @@ def print_results(results) -> str:
 
 
 results = retrieve()
-if results == "No Data.":
+if isinstance(results, str):
     print(results)
 else:
     print_results(results)
